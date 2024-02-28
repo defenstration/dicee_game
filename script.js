a7e47b8fc1185dc9eb13d6f291baf80a996580da
defenstration <41651534+defenstration@users.noreply.github.com>
2024-02-28 15:01:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.container');
+const die1 = document.querySelector('.dice');
+const die2 = document.querySelector('.dice:nth-child(2)');
+const rollBtn = document.querySelector('.roll-button');
+const img1 = document.querySelector('.img1');
+const img2 = document.querySelector('.img2');
+const h1 = document.querySelector("h1");
+
+const determineWinner = () => {
+    let die1roll = Math.floor(Math.random()*6) +1;
+    let die2roll = Math.floor(Math.random()*6)+1;
+    img1.setAttribute('src', `./images/dice${die1roll}.png`);
+    img2.setAttribute('src', `./images/dice${die2roll}.png`);
+    if (die1roll > die2roll) {
+        h1.textContent = "Player 1 Wins!";
+    } else if (die2roll > die1roll){
+        h1.textContent = "Player 2 Wins!";
+    } else if (die1roll === die2roll){
+        h1.textContent = "Draw!"
+    }
+    console.log(die1roll);
+}
+
+
+
+rollBtn.addEventListener('click', determineWinner)
